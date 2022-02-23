a8b1c807e27e1edb7644f6b69dfee21bbf80a44d
Daria Romanova <d.e.romanova@mail.ru>
2022-02-23 15:15:25 +0300

    Проект создан на Vue CLI с подключением Vue Router

---
+++ ДЗ8/project_vue_router/src/components/CartItem.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="cart-item">
+        <div class="product-bio">
+            <img src="https://picsum.photos/50/50" alt="Some image">
+                <div class="product-desc">
+                    <h1 class="product-title pinktext">{{cartItem.product_name}}</h1>
+                    <p class="product-quantity">Quantity: {{cartItem.quantity}}</p>
+                    <p class="product-single-price">Price: {{cartItem.price}}</p>
+                </div>
+        </div>
+        <div class="right-block">
+            <p class="product-price">Total: {{cartItem.quantity*cartItem.price}}</p>
+            <button class="del-btn" @click="$emit('remove', cartItem)">&times;</button>
+         </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CartItem",
+    props: ['cartItem'],
+};
+</script>
